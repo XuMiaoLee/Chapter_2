19e29df9433c5761a17cb9a00ccb13191f9534f5
xumiao <1576029838@qq.com>
2018-01-18 09:34:32 +0800

    binderpool

--- app/src/main/java/com/xyj/chapter_2/binderpool/BinderPoolActivity.kt
+++ app/src/main/java/com/xyj/chapter_2/binderpool/BinderPoolActivity.kt
@@ -3,6 +3,7 @@ package com.xyj.chapter_2.binderpool
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.xyj.chapter_2.R
+import com.xyj.chapter_2.utils.LogUtils
 
 class BinderPoolActivity : AppCompatActivity() {
 
@@ -10,8 +11,20 @@ class BinderPoolActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_binder_pool)
 
-        Thread{
-            val compute = BinderPool.getInstance(this)?.queryBinder(BinderPool.BINDER_COMPUTE) as ICompute
+        //这边要开线程处理，因为BinderPool中用了CountDownLatch，可能会阻塞UI线程
+        Thread {
+            val binderPool = BinderPool.getInstance(this)
+            val computeBinder = binderPool?.queryBinder(BinderPool.BINDER_COMPUTE)
+            val compute = ICompute.Stub.asInterface(computeBinder)
+            val result = compute.add(1, 2)
+            LogUtils.d("1 + 2 = $result")
+
+            val securityBinder = binderPool?.queryBinder(BinderPool.BINDER_SECURITY_CENTER)
+            val security = ISecurityCenter.Stub.asInterface(securityBinder)
+            val encrypt = security.encrypt("android")
+            LogUtils.d("encrypt=$encrypt")
+            val decrypt = security.decrypt(encrypt)
+            LogUtils.d("decrypt=$decrypt")
         }.start()
     }
 }
